a98add9196c4269f332bace943cccae6de508156
streamingturtles.com <pcardillo@streamingturtles.com>
2021-02-06 23:56:40 -0500

    added db scema & seed sql files as well as  README.md placeholder file

---
+++ db/db.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30), 
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30), 
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
